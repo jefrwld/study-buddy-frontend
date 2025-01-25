f04a34072660fcf54326b95bb0b6bd4233758b95
jefrwld <jeffrey.freiwald98@gmail.com>
2025-01-25 15:06:33 +0100

    http client bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
+import { NotedApiService } from './noted-api.service';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'notedFrontend';
+  selectedFiles: File[] = [];
+  constructor(private notedApiService: NotedApiService) {}
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files) {
+      this.selectedFiles = Array.from(input.files);
+    }
+  }
+
+  onSubmit(event: Event): void {
+    event.preventDefault();
+
+    if (this.selectedFiles.length > 0) {
+      this.notedApiService.uploadFiles(this.selectedFiles).subscribe(
+        response => {
+          console.log('Upload erfolgreich:', response); 
+        },
+        error => {
+          console.error('Upload fehlgeschlagen:', error);
+        }
+      );
+    }
+  }
 }
--- src/app/noted-api.service.ts
+++ src/app/noted-api.service.ts
@@ -1,20 +1,18 @@
-import {Injectable} from '@angular/core';
-import {ImageInterpretation} from './response-interfaces';
-import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root', // Stellt den Service global bereit
 })
-
 export class NotedApiService {
-    private baseUrl: string = 'http://localhost:8080';
+  private baseUrl: string = 'http://localhost:8080';
 
-    constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-    getImageInterpretation(UplaodFile: File): Observable<ImageInterpretation[]> {
-        return this.http.get<ImageInterpretation[]>(`${this.baseUrl}/upload/${UplaodFile}`);
-    }
+  uploadFiles(files: File[]): Observable<any> {
+    const formData = new FormData();
+    files.forEach(file => formData.append('file[]', file, file.name));
+    return this.http.post(`${this.baseUrl}/upload`, formData);
+  }
 }
-
-
