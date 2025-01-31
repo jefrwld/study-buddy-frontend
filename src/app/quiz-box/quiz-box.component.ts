cec7372e7a4ec5fa6e5b8b627c1f92ec25ed13f0
jefrwld <jeffrey.freiwald98@gmail.com>
2025-01-31 13:35:58 +0100

    start way of just showing one card at a time

--- src/app/quiz-box/quiz-box.component.ts
+++ src/app/quiz-box/quiz-box.component.ts
@@ -14,11 +14,13 @@ export class QuizBoxComponent {
   @Input({ required: true }) questions!: Signal<any[]>;
 
   selectedAnswers: any[] = [];  // Speichert die Antworten des Benutzers
+  visibleQuestion: number = 0;
 
   // Vergleiche die Antworten und gib Feedback
   checkAnswer(questionIndex: number, selectedAnswer: string) {
     const correctAnswer = this.questions()[questionIndex].correct;
     if (selectedAnswer === correctAnswer) {
+      this.visibleQuestion=questionIndex;
       alert('Correct!');
     } else {
       alert('Incorrect!');
